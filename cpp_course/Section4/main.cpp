6bd91205043705ce6eb0519e8cfe733988e446ed
stepanskyvlad <vlad14yarema@gmail.com>
2024-11-25 13:13:12 +0200

    add cpp course folder

---
+++ cpp_course/Section4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int fav_number {100};
+
+    std::cout << "Hello, Section4" << std::endl;
+
+    std::cout << "Enter your favorite number: ";
+    std::cin >> fav_number;
+    std::cout << "Your favorite number is: " << fav_number << std::endl;
+
+    return 0;
+}
---
+++ cpp_course/Section5/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Section5" << std::endl;
+    return 0;
+}
