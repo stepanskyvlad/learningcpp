a5774b68d95053c9114a87abc43b791cdec4c4cb
stepanskyvlad <vlad14yarema@gmail.com>
2025-12-23 20:42:19 +0200

    🔧 chore: add a new lesson

---
+++ cpp_course/polymorphism/using_override/main.cpp
@@ -0,0 +1,38 @@
+// Section 16
+// Using override
+#include <iostream>
+
+class Base {
+public:
+    virtual void say_hello() const {
+        std::cout << "Hello - I'm a Base class object" << std::endl;
+    }
+    virtual ~Base() {}
+};
+
+class Derived: public Base {
+public:
+    // virtual void say_hello()                 {             // Notice I forgot the const
+    // virtual void say_hello() override {             // Notice I forgot the const. Compile error - override helps to detect it
+    virtual void say_hello()  const override {
+        std::cout << "Hello - I'm a Derived class object" << std::endl;
+    }
+    virtual ~Derived() {}
+};
+
+
+int main() {
+    
+    Base *p1 = new Base();      // Base::say_hello()
+    p1->say_hello();
+    
+    Derived *p2 = new Derived();    // Derived::say_hello()
+    p2->say_hello();
+    
+    Base *p3 = new Derived();   //  Base::say_hello()   ?????   I wanted Derived::say_hello()
+    p3->say_hello();
+    
+   
+    return 0;
+}
+
