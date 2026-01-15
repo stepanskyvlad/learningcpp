f65308c2d3191c47f48fd74dc8ce58459c3f9702
stepanskyvlad <vlad14yarema@gmail.com>
2026-01-15 22:07:21 +0200

    🔧 chore: add two finished tasks

--- cpp_course/challenges/io_and_stream/Challenge_2/main.cpp
+++ cpp_course/challenges/io_and_stream/Challenge_2/main.cpp
@@ -2,13 +2,48 @@
 // Challenge 2
 // Automated Grader
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <numeric>
 
-using namespace std;
+int calc_score(std::string correct_answers, std::string student_answers) {
+    int score {0};
+    for (size_t i = 0; i < correct_answers.size(); i++) {
+        if (correct_answers.at(i) == student_answers.at(i)) {
+            score += 1;
+        }
+    }
+    return score;
+}
 
 int main() {
-    
-    
-    cout << "Hello world" << endl;
+    std::ifstream file;
+    std::string correct_score;
+    std::string student_score;
+    std::string student_name;
+    std::vector<int> scores {};
+
+    file.open("responses.txt");
+    if (!file) {
+        std::cerr << "Problem with reading files." << std::endl;
+        return 1;
+    }
+    file >> correct_score;
+    std::cout << "Name" << std::setw(36) << "Score" << std::right << std::endl;
+    std::cout << std::setfill('-') << std::setw(40) << "" << std::endl;
+    std::cout << std::setfill(' ');
+    while (file >> student_name >> student_score) {
+        scores.push_back(calc_score(correct_score, student_score));
+        std::cout << student_name << std::setw(40 - student_name.size()) << calc_score(correct_score, student_score) << std::right << std::endl;
+    }
+    std::cout << std::setfill('-') << std::setw(40) << "" << std::endl;
+    std::cout << std::setfill(' ');
+    float average = std::accumulate(scores.begin(), scores.end(), 0.0f) / scores.size();
+    std::cout << "Average score" << std::setw(27) << average << std::right << std::endl;
+
+
     return 0;
 }
 
--- cpp_course/challenges/io_and_stream/Challenge_3/main.cpp
+++ cpp_course/challenges/io_and_stream/Challenge_3/main.cpp
@@ -2,13 +2,42 @@
 // Challenge 3
 // Word counter
 #include <iostream>
+#include <fstream>
+#include <cctype>
 
-using namespace std;
-
-int main() {
-    
-    
-    cout << endl;
-    return 0;
+void to_lowercase_string(std::string& s) {
+    for (char& c: s) {
+        c = std::tolower(static_cast<unsigned char>(c));
+    }
 }
 
+int main() {
+    std::ifstream file;
+    int word_counter = 0;
+    std::string user_word;
+    int match_counter = 0;
+    std::cout << "Enter the substring to search for: ";
+    std::cin >> user_word;
+    to_lowercase_string(user_word);
+
+    file.open("romeoandjuliet.txt");
+    if (!file) {
+        std::cerr << "Problem opening file" << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (file >> word) {
+        word_counter++;
+        std::string lowered = word;
+        to_lowercase_string(lowered);
+        if (lowered.find(user_word) != std::string::npos) {
+            match_counter++;
+        }
+    }
+
+    std::cout << word_counter << " words were searched..." << std::endl;
+    std::cout << "The substring '" << user_word << "' was found " << match_counter << " times" << std::endl;
+
+    return 0;
+}
